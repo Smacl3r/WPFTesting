7ef00ea72995631bc0e4d5198f2aa62d3da9e926
DESKTOP-1CNF0H0\Dalius <dalius.zukauskas@gmail.com>
2019-06-30 22:12:12 +0300

    Wpf test app

---
+++ 03-TreeViewsSimpleViewModel/WpfTreeView/HeaderToImage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Converts a full path to a specific image type of a drive, folder or file
+    /// </summary>
+    [ValueConversion(typeof(DirectoryItemType), typeof(BitmapImage))]
+    public class HeaderToImage : IValueConverter
+    {
+        public static HeaderToImage Instance = new HeaderToImage();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // By default we presume it is a file
+            var image = "Images/file.png";
+
+            switch ((DirectoryItemType)value)
+            {
+                case DirectoryItemType.Drive:
+                    image = "Images/drive.png";
+                    break;
+                case DirectoryItemType.Folder:
+                    image = "Images/folder.png";
+                    break;
+            }
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{image}.png"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPFDirView/WPFDirView/WPFDirView/HeaderToImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using WpfDirView;
+
+namespace WPFDirView
+{
+    /// <summary>
+    /// Converts a full path to a specific image type of a drive, folder or file
+    /// </summary>
+    [ValueConversion(typeof(DirectoryItemType), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{value}.png"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
